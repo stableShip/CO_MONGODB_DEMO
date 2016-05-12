e998ad679e076e4cecb9a3fd5aed7884bc71e114
stableShip <296888680@qq.com>
2016-05-12 11:17:28 +0800

    添加 find in select , sort 测试

--- base_crud.js
+++ base_crud.js
@@ -4,7 +4,7 @@ var co = require("co");
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 var should = require("should");
-
+var _ = require("lodash");
 var userSchema = new Schema({
     name: String,
     gender: String,
@@ -14,6 +14,7 @@ var userSchema = new Schema({
         name: String,
     },
     crtTime: {type: Date, default: new Date()}
+
 });
 
 describe("base_crud", function () {
@@ -49,7 +50,7 @@ describe("base_crud", function () {
                 user = yield User.findOne({name: "JIE"});
                 (user === null).should.be.true;
                 done();
-            }catch (err){
+            } catch (err) {
                 console.log(err);
             }
         });
@@ -63,14 +64,14 @@ describe("base_crud", function () {
                 user.name = "JIE"
                 yield user.save();
                 user = yield User.findOne({name: "JIE"});
-                user.name="test";
+                user.name = "test";
                 yield user.save()
                 user = yield User.findOne({name: "JIE"});
                 (null === user).should.be.true;
                 user = yield User.findOne({name: "test"});
                 user.name.should.exactly("test")
                 done();
-            }catch (err){
+            } catch (err) {
                 console.log(err);
             }
         });
@@ -87,7 +88,7 @@ describe("base_crud", function () {
                 user.should.be.an.ARRAY;
                 user[0].name.should.exactly("JIE")
                 done();
-            }catch (err){
+            } catch (err) {
                 console.log(err);
             }
         });
@@ -103,7 +104,93 @@ describe("base_crud", function () {
                 user = yield User.findOne({name: "JIE"});
                 user.name.should.exactly("JIE")
                 done();
-            }catch (err){
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    })
+
+
+    it("find in", function (done) {
+        co(function* () {
+            try {
+                yield User.create({
+                    role: {
+                        name: "1"
+                    }
+                }, {
+                    role: {
+                        name: "2"
+                    }
+                }, {
+                    role: {
+                        name: "3"
+                    }
+                }, {
+                    role: {
+                        name: "4"
+                    }
+                }, {
+                    role: {
+                        name: "5"
+                    }
+                }, {
+                    role: {
+                        name: "6"
+                    }
+                }, {
+                    role: {
+                        name: "1"
+                    }
+                }, {
+                    role: {
+                        name: "1"
+                    }
+                }, {
+                    role: {
+                        name: "1"
+                    }
+                })
+                var users = yield User.find({"role.name": {$in: [1, 2, 3]}}).skip(2).limit(2);
+                console.log(users)
+                users.should.be.an.Array;
+                done();
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    })
+
+
+    it("sort", function (done) {
+        co(function* () {
+            try {
+                yield User.create({name: 1}, {name: 2}, {name: 3}, {name: 1}, {name: 5}, {name: 6}, {name: 1})
+                var user = yield User.findOne({name: 1}).sort({_id: -1});
+                user.name.should.exactly("1")
+                done();
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    })
+
+
+    it("find select", function (done) {
+        co(function* () {
+            try {
+                yield User.create({
+                    name: "JIE",
+                    role: {
+                        name: "1"
+                    },
+                    gender: "1"
+                })
+                var users = yield User.find({"name": "JIE"}).select({name: 1}).skip(0).limit(2);
+                console.log(users)
+                users.should.be.an.Array;
+                done();
+            } catch (err) {
                 console.log(err);
             }
         });
